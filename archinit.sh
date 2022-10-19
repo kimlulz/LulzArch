a3da7872c1925c4a94b21a4a3864df67621026fe
kimlulz <kimlulz@naver.com>
2022-10-19 13:54:32 +0900

    Updated with adding script

---
+++ archinit.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+BL=$(tput bold) # 굵게
+NRM=$(tput sgr0) # 일반글씨
+RW="\033[41;37m" # 빨간색 배경 글씨
+NRW="\033[0m" #일반 배경 글씨
+
+function becho {
+	>&2 echo -n "$BL$1$NRM"
+	echo ""
+}
+
+function ach {
+	>&2 arch-chroot /mnt $1
+	echo ""
+}
+
+function check_fail {
+	if [[ $1 -ne 0 ]]; then
+		>&2 echo "FAIL!"
+		exit 1
+	else
+		>&2 echo "OK!"
+	fi
+}
+
+becho "0. Checking internet connectivity... "
+	pacman -Sy curl wget
+	wget -q --tries=10 --timeout=20 --spider http://www.google.com
+	check_fail $?
+echo ""
+
+
+becho "1. Make Partition Table... "
+	becho "-1. Select Disk ex)sda / nvme0n1"
+		becho "***** [/boot] *****"
+				lsblk
+		becho "**************************"
+		echo -n " > "; read "seldisk"
+		cfdisk /dev/$seldisk
+
+	becho "-2. Format ex)sda1 / nvme0n1p1"
+		becho "***** [/boot] *****"
+				lsblk
+		becho "**************************"
+		echo -n " > "; read "selboot"
+		mkfs.xfs -f /dev/$selboot
+		echo ""
+
+		becho "***** [/boot/efi] *****"
+				lsblk
+		becho "**************************"
+		echo -n " > "; read "selefi"
+		mkfs.vfat -F32 /dev/$selefi
+		echo ""
+
+		becho "***** [/] *****"
+				lsblk
+		becho "**************************"
+		echo -n " > "; read "selroot"
+		mkfs.xfs -f /dev/$selroot
+		echo ""
+
+
+	becho "-3 Mount"
+		becho "Mount root"
+		mount /dev/$selroot /mnt
+
+		becho "Mount /boot"
+		mkdir /mnt/boot
+		mount /dev/$selboot /mnt/boot
+
+		becho "Mount /boot/efi"
+		mkdir /mnt/boot/efi
+		mount /dev/$selefi /mnt/boot/efi	
+echo ""
+
+becho "2. Init System"
+	pacstrap /mnt base linux-zen linux-zen-headers linux-firmware nano networkmanager base-devel man-db man-pages texinfo dosfstools e2fsprogs
+	genfstab -U /mnt >> /mnt/etc/fstab
+echo ""
+
+becho "3. Copy for setting on chroot"
+	if [ -f ./archset.sh ]; then
+		cp ./archset.sh /mnt
+		arch-chroot /mnt
+	else
+		wget https://raw.githubusercontent.com/kimlulz/lulzarch/main/archset.sh
+		cp ./archset.sh /mnt
+		arch-chroot /mnt
+	fi
+becho "Type ./archset.sh"
+#arch-chroot /mnt ./archset.sh
---
+++ archset.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+BL=$(tput bold) # 굵게
+NRM=$(tput sgr0) # 일반글씨
+RW="\033[41;37m" # 빨간색 배경 글씨
+NRW="\033[0m" #일반 배경 글씨
+
+function becho {
+	>&2 echo -n "$BL$1$NRM"
+	echo ""
+}
+
+becho "3. Setting System"
+	becho "-1. Init"
+		pacman -S noto-fonts-cjk sudo grub efibootmgr reflector ibus-hangul
+		reflector --country 'South Korea' --save /etc/pacman.d/mirrorlist
+	echo ""
+	becho "-2. Set root password"
+		passwd
+	echo ""
+	becho "-3. Set Language to KR"
+		sed -i 's/#ko_KR.UTF-8/ko_KR.UTF-8/g' /etc/locale.gen
+		locale-gen
+		echo LANG=en_US.UTF-8 > /etc/locale.conf
+		export LANG=ko_KR.UTF-8
+	echo ""
+	becho "-4. Change hostname"
+		echo -n " > "; read "HOSTNAME"
+		echo $HOSTNAME > /etc/hostname
+	echo ""
+	becho "-5. Change timezone to KR"	
+		ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+		hwclock --systohc
+	echo ""
+	becho "-6. Make Account"	
+		echo -n " > "; read "USER"
+		useradd -m -g users -G wheel -s /bin/bash $USER
+		echo "Input password"
+		passwd $USER
+	becho "-7 Update Packages"
+		pacman -Syu
+	becho "-7 Update sudoer"
+		sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+		echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo ""
+
+becho "4. Install GRUB"
+	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch --recheck
+	grub-mkconfig -o /boot/grub/grub.cfg
+	systemctl enable NetworkManager
+echo ""
+
+becho "5. Install gnome"
+	sudo pacman -S xorg-xwayland gnome gnome-extra
+	sudo systemctl enable gdm
+echo ""
+
+becho "6. Install yay and some packages"
+	pacman -S --needed git base-devel
+	cd /opt/
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	cd /
+	yay -S naver-whale-stable
+echo ""
+
+rm -rf ./archset.sh
+becho "Finished!!"
+
+exit
+umount -lr /mnt
